f2038c4040ac0ad95dab21d369f21e64b10c7434
root <root@HelloAmy.x1hsg4m4ohvubkzohoa42g2ajg.ix.internal.cloudapp.net>
2017-01-24 09:37:36 +0000

    Face Detection Done

--- faceDetect.py
+++ faceDetect.py
@@ -3,12 +3,12 @@ import dlib
 from skimage import io
 
 # Take the image file name from the command line
-file_name = sys.argv[1]
+file_name = "1.jpg"
 
 # Create a HOG face detector using the built-in dlib class
 face_detector = dlib.get_frontal_face_detector()
 
-win = dlib.image_window()
+#win = dlib.image_window()
 
 # Load the image into an array
 image = io.imread(file_name)
@@ -20,7 +20,7 @@ detected_faces = face_detector(image, 1)
 print("I found {} faces in the file {}".format(len(detected_faces), file_name))
 
 # Open a window on the desktop showing the image
-win.set_image(image)
+#win.set_image(image)
 
 # Loop through each face we found in the image
 for i, face_rect in enumerate(detected_faces):
@@ -30,7 +30,7 @@ for i, face_rect in enumerate(detected_faces):
 	print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
 
 	# Draw a box around each face we found
-	win.add_overlay(face_rect)
+#	win.add_overlay(face_rect)
 	        
 # Wait until the user hits <enter> to close the window	        
 dlib.hit_enter_to_continue()
