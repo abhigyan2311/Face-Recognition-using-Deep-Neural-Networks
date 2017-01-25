c6c10b7358043d05853e19a9e0b3f2efc57a5a42
Abhigyan Singh <abhigyan2311@gmail.com>
2017-01-26 00:43:37 +0530

    Python call

--- faceRecog.py
+++ faceRecog.py
@@ -2,7 +2,8 @@ import os
 import subprocess
 
 s=subprocess.check_output(['./classifier.py', 'infer', 'generated-embeddings/classifier.pkl', 'test/1.jpg'])
-print s
+
 resArr=s.split()
-print resArr[0]
-print resArr[1]
+nameArr = resArr[0].split('-')
+name = nameArr[0] + ' ' + nameArr[1]
+conf = resArr[1]
