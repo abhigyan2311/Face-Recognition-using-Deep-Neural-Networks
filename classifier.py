3212df7adc7fce2d455e8d64ba3dec2cf91ed943
Abhigyan Singh <Abhigyan@Abhigyans-MacBook-Pro.local>
2017-01-25 18:24:36 +0530

    Multiple faces detection

--- classifier.py
+++ classifier.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python2
+#
+# Example to classify faces.
+# Brandon Amos
+# 2015/10/11
+#
+# Copyright 2015-2016 Carnegie Mellon University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import time
 
 start = time.time()
@@ -25,7 +45,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 
 fileDir = os.path.dirname(os.path.realpath(__file__))
-modelDir = os.path.join(fileDir, 'models')
+modelDir = os.path.join(fileDir, '..', 'models')
 dlibModelDir = os.path.join(modelDir, 'dlib')
 openfaceModelDir = os.path.join(modelDir, 'openface')
 
@@ -150,7 +170,7 @@ def train(args):
         pickle.dump((le, clf), f)
 
 
-def infer(args, multiple=False):
+def infer(args, multiple=True):
     with open(args.classifierModel, 'r') as f:
         (le, clf) = pickle.load(f)
 
