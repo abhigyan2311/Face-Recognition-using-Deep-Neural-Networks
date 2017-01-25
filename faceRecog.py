18d987359518609568ca3c7d40c765d31ca5bc0c
Abhigyan Singh <Abhigyan@Abhigyans-MacBook-Pro.local>
2017-01-26 00:30:20 +0530

    Python call

--- faceRecog.py
+++ faceRecog.py
@@ -1,4 +1,6 @@
 import os
 import subprocess
 
-subprocess.call(['./classifier.py', 'infer', 'generated-embeddings/classifier.pkl', 'test/1.jpg'])
+s=subprocess.check_output(['./classifier.py', 'infer', 'generated-embeddings/classifier.pkl', 'test/1.jpg'])
+print "OK"
+print s
