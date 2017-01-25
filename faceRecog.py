28c03306f4cea3f1064011e7c1cf7e31e15f433e
Abhigyan Singh <abhigyan2311@gmail.com>
2017-01-26 00:56:43 +0530

    Python recog call done

--- faceRecog.py
+++ faceRecog.py
@@ -1,12 +1,14 @@
 import os
 import subprocess
 
-s=subprocess.check_output(['./classifier.py', 'infer', 'generated-embeddings/classifier.pkl', 'test/1.jpg'])
+def recog(img):
+    imgPath='test/' + img
+    s=subprocess.check_output(['./classifier.py', 'infer', 'generated-embeddings/classifier.pkl', imgPath])
+    resArr=s.split()
+    nameArr = resArr[0].split('-')
+    name = nameArr[0] + ' ' + nameArr[1]
+    conf = resArr[1]
+    return (name,conf)
 
-resArr=s.split()
-nameArr = resArr[0].split('-')
-name = nameArr[0] + ' ' + nameArr[1]
-conf = resArr[1]
-
-print name
-print conf
+def train:
+    
