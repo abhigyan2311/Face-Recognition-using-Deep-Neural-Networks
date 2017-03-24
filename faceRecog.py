e57a3df5a672cbe3af7129eb5a73c8a121274f9c
Abhigyan Singh <abhigyan2311@gmail.com>
2017-03-24 14:39:35 +0530

    Pubnub listner

--- faceRecog.py
+++ faceRecog.py
@@ -5,7 +5,7 @@ import urllib
 def recog(img):
     urllib.urlretrieve("https://s3-ap-southeast-1.amazonaws.com/hellomark/capturedImg.jpg", "test/capturedImg.jpg")
     imgPath='test/capturedImg.jpg'
-    s=subprocess.check_output(['./classifier.py', 'infer', 'generated-embeddings/classifier.pkl', imgPath])
+    s=subprocess.check_output(['classifier.py', 'infer', 'generated-embeddings/classifier.pkl', imgPath])
     resArr=s.split()
     nameArr = resArr[0].split('-')
     name = nameArr[0] + ' ' + nameArr[1]
