fa094d4d5d19b3bc343c74bcb285abac5a44f8ca
Abhigyan Singh <abhigyan2311@gmail.com>
2017-03-24 14:40:54 +0530

    Pubnub listner

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ class MySubscribeCallback(SubscribeCallback):
 
     def message(self, pubnub, message):
         if(message.channel == 'faceCapture'):
-            s=subprocess.check_output(['faceRecog.py', 'recog'])
+            s=subprocess.check_output(['./faceRecog.py', 'recog'])
             print(s)
 
 
