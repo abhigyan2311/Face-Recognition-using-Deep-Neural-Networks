140f6aa07249a151aa4a5f68c20b5ea593e342cb
Abhigyan Singh <abhigyan2311@gmail.com>
2017-03-24 21:31:32 +0530

    Test pic

--- main.py
+++ main.py
@@ -13,10 +13,9 @@ pnconfig.publish_key = "pub-c-73cca4b9-e219-4f94-90fc-02dd8f018045"
 pnconfig.secret_key = "sec-c-YzcyZjI4NzEtNmUxMi00ZDc0LWI4ZGMtNGUyYmFlZmI1OTQ3"
 pnconfig.ssl = False
 
-path = './'+path+'.jpg'
-access_token = 'EAAO1qxWMufsBAMYl0IUWKnMhQ2ebDMjtlm5Oh9LB4U6Fbp13U1ewGzSKPovxiWUg3OH7q9YG39bQwGVlxeXjJKi7BonaggZCSzAwn1pHMX3HZAtE34XiLBf1AddBys4mCUErDbkIwJ9f9QFzZBV3yIP9jzubbMk4N3mw4EX8wZDZD'
-cookie = 'datr=dfRTWMGsPA7exeoXIj2LX43x; sb=-PRTWLHlqlWfTjtD62dzO59p; c_user=100002221569995; xs=11%3AO5u2NHgGlMHFKQ%3A2%3A1481897208%3A4831; fr=0AlYydX1C7b1JYXjo.AWVrlN-NQoFxwO-PJS4qyq4MscE.BYI2pk.Bn.AAA.0.0.BYU_T4.AWWKuXn1; csm=2; s=Aa6AwfHffG_4nCdT.BYU_T4; pl=n; lu=ggFeRlLj9_Xkmmb3xPIA46SA; act=1481897312600%2F1; p=-2; presence=EDvF3EtimeF1481897390EuserFA21B02221569995A2EstateFDutF1481897390118CEchFDp_5f1B02221569995F4CC;'
-fb_dtsg = 'AQG9AGMpnGmx%3AAQF8DLfTx81k' #Insert the fb_dtsg parameter obtained from Form Data here.
+access_token = 'EAAO8ErKT0tkBAMZAvvgsKAmg4OIXyAjM2CeUTrAQeib26eSGLlcWkIbY7BK5wreglZC6GiZBqDi8gyOOgfzSSz'
+cookie = 'datr=dfRTWMGsPA7exeoXIj2LX43x; sb=-PRTWLHlqlWfTjtD62dzO59p; c_user=100002221569995; xs=1%3Ad2iAeVIJVH813Q%3A2%3A1490370805%3A4831; fr=0AlYydX1C7b1JYXjo.AWW21x2w4q6cKw-Q-g20bDm-03Q.BYI2pk.Bn.Fh8.0.0.BY1UD1.AWVHr2k9; csm=2; pl=n; lu=gghjXVbPCttQrj2bVnBiUC1w; presence=EDvF3EtimeF1490370848EuserFA21B02221569995A2EstateFDutF1490370848557CEchFDp_5f1B02221569995F3CC'
+fb_dtsg = 'AQFR6XCnO_c6:AQF1CkRDYXg_' #Insert the fb_dtsg parameter obtained from Form Data here.
 
 pubnub = PubNub(pnconfig)
 
@@ -85,12 +84,13 @@ class MySubscribeCallback(SubscribeCallback):
             if(conf>75):
                 pubnub.publish().channel('faceRecog').message([name]).sync()
             else:
+                path='test/capturedImg.jpg'
                 for line in sys.stdin:
                     path = line[:-1]
                 print(recognize(path,access_token,cookie,fb_dtsg))
 
 
-            
+
 
 
 pubnub.add_listener(MySubscribeCallback())
